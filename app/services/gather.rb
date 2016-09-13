9804ff0e848e7b02732f16a07020a84fe98afbb5
Reece <reecebisel@gmail.com>
2016-09-12 22:49:09 -0600

    update gather.rb to help create thoughts
    this is the class that should get the thoughts from the API and record
    them to the database. I plan on having this called from a rake task

--- app/services/gather.rb
+++ app/services/gather.rb
@@ -1,6 +1,13 @@
 class Gather
+  include HTTParty
 
-  def initialize
+  BASE_URi = 'https://www.reddit.com/r/'
+  PAGE_LIMIT = 25
+
+  def initialize(amount)
+    @amount = amount
+    @count = 0
+    @pages = 5
   end
 
   def self.perform
@@ -8,7 +15,42 @@ class Gather
   end
 
   def perform
+    get_new_thoughts
+    parse_data
+    create_thoughts
+  rescue => e.message
+    # figure out what to do here. It might not be needed not sure yet
   end
 
+  private
   
+  attr_accessor :options, :thoughts, :count, :after, :parsed
+
+  def get_new_thoughts
+    @thoughts = self.class.get('/ShowerThoughts/new.json', options)
+    @count    = count + PAGE_LIMIT 
+    @after    = @thoughts['data']['after']
+    @options  = { count: @count, after: @after }   
+  end
+
+  def parse_data
+    @parsed = []
+
+    @thoughts['children'].each do |child|
+      new_thought = Hash.new { |hash, key| hash[key] = nil }
+      new_thought[:title]     = child['data']['title']
+      new_thought[:text]      = child['data']['selftext']
+      new_thought[:url]       = child['data']['url']
+      new_thought[:subreddit] = child['data']['subreddit']
+      @parsed << new_thought
+    end
+
+    @parsed
+  end
+
+  def create_thoughts
+    @parsed.each do |thought_params|
+      Thought.create!(thought_params)
+    end
+  end
 end
