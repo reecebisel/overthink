5ee24dfab133bbff9d90834d49b2e0511d94d5c9
Reece <reecebisel@gmail.com>
2016-09-26 23:48:26 -0600

    add comments about possible alternitve to reddit api

--- app/services/gather.rb
+++ app/services/gather.rb
@@ -51,4 +51,10 @@ class Gather
   def thought_attributes
     @thought_attributes ||= Thought.new.attributes.except('created_at', 'updated_at').keys
   end
+
+  # depending on the oauth stuff for reddit. I could end up using this as a joke generator. 
+  # this is a joke API the seems pretty simple.
+  # http://www.programmableweb.com/api/webknox-jokes 
+  # I would really prefer to use the Reddit API
+  # that way I can build my own news feed from all the subreddits I actually care about
 end
